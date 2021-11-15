9bb088d5182ee3164ed3cff7e35b82de5c806e75
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2021-11-15 17:38:41 +1000

    Moved CarData to correct folder
    Started front end code

--- src/commonMain/kotlin/CarData.kt
+++ src/commonMain/kotlin/CarData.kt
@@ -4,11 +4,6 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import java.util.UUID
 
-
-
-
-
-
 @Serializable
 data class CarData(val make: String, val model: String ,val colour: String, val numberPlate: String, val description: String)
 {
