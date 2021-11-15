de2f715c47ddfa89e9f82da61c4266e8a686f92d
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2021-11-15 18:02:25 +1000

    Remove UUID
    Had to remove UUID for now since kotlin/js doesnt support java stuff will need to find another implementation later

--- src/commonMain/kotlin/CarData.kt
+++ src/commonMain/kotlin/CarData.kt
@@ -1,13 +1,10 @@
 import kotlinx.serialization.Serializable
-import HashFunctions
-import java.math.BigInteger
-import java.security.MessageDigest
-import java.util.UUID
+
 
 @Serializable
 data class CarData(val make: String, val model: String ,val colour: String, val numberPlate: String, val description: String)
 {
-    val uniqueID = UUID.randomUUID().toString()
+    var uniqueID = "temporary for now"
     companion object{
         const val path = "/carList"
     }
--- src/jsMain/kotlin/Api.kt
+++ src/jsMain/kotlin/Api.kt
@@ -4,7 +4,6 @@ import io.ktor.client.request.*
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 
-
 import kotlinx.browser.window
 
 val endpoint = window.location.origin // only needed until https://youtrack.jetbrains.com/issue/KTOR-453 is resolved
@@ -17,13 +16,13 @@ suspend fun getShoppingList(): List<CarData> {
     return jsonClient.get(endpoint + CarData.path)
 }
 
-suspend fun addShoppingListItem(shoppingListItem: ShoppingListItem) {
-    jsonClient.post<Unit>(endpoint + ShoppingListItem.path) {
+suspend fun addShoppingListItem(shoppingListItem: CarData) {
+    jsonClient.post<Unit>(endpoint + CarData.path) {
         contentType(ContentType.Application.Json)
         body = shoppingListItem
     }
 }
 
-suspend fun deleteShoppingListItem(shoppingListItem: ShoppingListItem) {
-    jsonClient.delete<Unit>(endpoint + ShoppingListItem.path + "/${shoppingListItem.id}")
+suspend fun deleteShoppingListItem(shoppingListItem: CarData) {
+    jsonClient.delete<Unit>(endpoint + CarData.path + "/${shoppingListItem.uniqueID}")
 }
